b8c185786667f10227cdfe7e51fb173731c12991
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-02-12 14:58:27 -0700

    Set up basic structure of proj

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import {Navbar} from "../components";
+
+const Home = () => {
+    return (
+      <>
+          <div className="min-h-screen">
+              <div className="gradient-bg-welcome">
+                  <Navbar />
+              </div>
+          </div>
+      </>
+    );
+};
+
+export default Home;

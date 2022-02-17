263a9534590a7e5e19e87d06f087e3aac7a75e2f
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-02-16 19:58:03 -0700

    Add context to application

--- client/src/components/welcome/welcome.jsx
+++ client/src/components/welcome/welcome.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
+import {TransactionContext} from "../../context/TransactionContext";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -18,6 +18,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+    const {value} = useContext(TransactionContext);
     // const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
 
     const handleSubmit = (e) => {
